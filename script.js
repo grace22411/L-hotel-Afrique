d2830c9f66a474ae19a0727d7b8f08d2704a2030
grace22411 <olayinkagrace112.ob@gmail.com>
2020-01-04 19:20:52 -0800

    gallery added

--- script.js
+++ script.js
@@ -64,25 +64,24 @@ $(document).ready(function(){
                 $(".expgroup").animate({height:'toggle'});
             });
 
-             /* activate jquery isotope */
-            var $container = $('#posts').isotope({
-                itemSelector : '.item',
-                isFitWidth: true
+            $('#navButton').click(function(){
+                $('.menu').slideToggle('slow');
+              togglemenu();
             });
-
-            $(window).smartresize(function(){
-                $container.isotope({
-                columnWidth: '.col-sm-3'
-                });
-            });
+          
+          $('.tabs').find('a').click( function (e) {
+            var theFilter = $(this).data('filter');
             
-            $container.isotope({ filter: '*' });
-
-                // filter items on button click
-            $('#filters').on( 'click', 'button', function() {
-                var filterValue = $(this).attr('data-filter');
-                $container.isotope({ filter: filterValue });
-            });
+            e.preventDefault();
+            $('.tabs').find('a').removeClass('active');
+            $(this).addClass('active');
+            
+           $('.portfolio').find('li').show();
+           if(theFilter != ".group-5")
+           {
+              $('.portfolio').find('li').not(theFilter).hide();
+           }
+          });
              
   
 });
@@ -99,5 +98,23 @@ function main(){
      }
          
      setInterval('main()', 5000);
+
+
+     function togglemenu() {
+        var div = document.getElementById("navButton");
+          
+        if (div.className == "closed") {open();}
+        else{close();}
+        }
+        
+        function open() {
+        document.getElementById("navButton").innerHTML="- Close -";
+        document.getElementById("navButton").className = "open";
+        }
+        
+        function close() {
+        document.getElementById("navButton").innerHTML="- Menu -";
+        document.getElementById("navButton").className="closed";
+        }
 
 
